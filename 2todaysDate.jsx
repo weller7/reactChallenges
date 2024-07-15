94f2386236a20a3d733d78d9a6288a93ea824d34
Jimena De Lara <40869710+weller7@users.noreply.github.com>
2024-07-15 16:46:43 -0600

    Create 2todaysDate.jsx

---
+++ 2todaysDate.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { getTodaysDate } from "./utils"
+
+function Today (){
+  return <p> Today is {getTodaysDate()} </p>
+}
+
+export default function App (){
+  return (
+    <Today />
+  )
+}

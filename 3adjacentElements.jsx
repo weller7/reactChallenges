41e82962cf45cef372dbc6b2a335cfbd5c9e317f
Jimena De Lara <40869710+weller7@users.noreply.github.com>
2024-07-15 17:00:05 -0600

    Create 3adjacentElements.jsx

---
+++ 3adjacentElements.jsx
@@ -0,0 +1,21 @@
+import "./styles.css";
+import * as React from "react"
+
+function Layout(){
+  return (
+    <>
+      <header> Header </header>
+      <main> main </main> 
+      <aside> Aside </aside>
+      <footer> FOOTER </footer> 
+    </>
+  )
+}
+
+export default function App() {
+  return (
+    
+      <Layout />
+    
+  );
+}
